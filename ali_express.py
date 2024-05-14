d10d2c3d94e4a347dedadfad65fd48bc9c2e8bf0
Adedotun-Adepoju <d.e.adepoju@gmail.com>
2024-05-14 15:47:26 +0100

    remove comments

--- ali_express.py
+++ ali_express.py
@@ -13,11 +13,8 @@ from selenium.webdriver.support import expected_conditions as EC
 # Python script to scrape product data from ALiExpress and Identify trending products bassed on the number of orders and reviews
 def scrape_products_by_category(category, num_products):
     chrome_options = Options()
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
     try: 
